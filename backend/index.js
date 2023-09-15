6c13ad156ff97841ccdecd2f6b7b41822b5de002
Maria Elena Melgarejo <melgarejo_maria@hotmail.com>
2023-09-15 10:25:29 -0300

    Instalación y configuracion de sequelize

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const db = require('./src/db');
+const sequelize = db.sequelize;
+
+try {
+    sequelize.authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+        });
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
---
+++ backend/src/db/index.js
@@ -0,0 +1,32 @@
+const { Sequelize } = require("sequelize")
+const mysql = require('mysql2');
+const dbConfig = require("./config/config.json")
+
+module.exports = db = {};
+console.log(dbConfig);
+// create db if it doesn't already exist
+const { host, port, user, password, database, dialect } = dbConfig.development;
+const pool = mysql.createPool({ host, port, user, password });
+pool.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+// connect to db
+const sequelize = new Sequelize(database, user, password, {
+    host,
+    port,
+    dialect,
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle
+    }
+});
+
+db.sequelize = sequelize;
+
+const User = require('./models/user')
+
+db.User = User;
+
+// sync all models with database
+sequelize.sync({ force: true });
