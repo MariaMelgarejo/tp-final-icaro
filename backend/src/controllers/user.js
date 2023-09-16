667a55944f2df9fb887796d9e65d5201c676b97a
Maria Elena Melgarejo <melgarejo_maria@hotmail.com>
2023-09-16 15:02:57 -0300

    User: get by ID agregado

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -4,7 +4,7 @@ const asyncHandler = require('express-async-handler')
 // Get all users
 const getUsers = asyncHandler(async (req, res) => {
     const users = await models.User.findAll({
-        attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
+        attributes: { exclude: ['password', 'updatedAt'] },
         include: [
             {
                 model: models.Address,
@@ -19,6 +19,25 @@ const getUsers = asyncHandler(async (req, res) => {
     res.status(200).json(users);
 });
 
+// Get user by ID
+const getUserById = asyncHandler(async (req, res) => {
+    const user = await models.User.findByPk(req.params.id, {
+        attributes: { exclude: ['password', 'updatedAt'] },
+        include: [
+            {
+                model: models.Address,
+                attributes: { exclude: ['id', 'userId', 'createdAt', 'updatedAt'] }
+            },
+            {
+                model: models.Contact,
+                attributes: { exclude: ['id', 'userId', 'createdAt', 'updatedAt'] }
+            },
+        ]
+    });
+    res.status(200).json(user);
+});
+
 module.exports = {
     getUsers,
+    getUserById
 }
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -5,4 +5,7 @@ const userController = require('../controllers/user');
 // Get all users
 router.get('/', userController.getUsers);
 
+// Get user by id
+router.get('/:id', userController.getUserById);
+
 module.exports = router;
