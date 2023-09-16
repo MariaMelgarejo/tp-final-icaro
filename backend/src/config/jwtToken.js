18dd5d11fc7dc30e15ba103dd57b95ef7093d9f5
Daniel Paduani <danielpdn04@gmail.com>
2023-09-16 12:30:51 -0300

    console.log borrado

--- backend/src/config/jwtToken.js
+++ backend/src/config/jwtToken.js
@@ -1,6 +1,4 @@
 const jwt = require('jsonwebtoken');
-console.log(process.env.JWT_SECRET);
-console.log(process.env.JWT_EXPIRES_IN);
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
