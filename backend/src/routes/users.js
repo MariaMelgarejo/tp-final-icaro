b534e5b1390cb2007cac098ba3d0b14625c3606c
Daniel Paduani <danielpdn04@gmail.com>
2023-09-16 15:12:24 -0300

    auth y admin middlewares agregados

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
+const { AuthMiddleware, isAdmin } = require('../middlewares/auth');
 
 // Get all users
-router.get('/', userController.getUsers);
+router.get('/', AuthMiddleware, isAdmin, userController.getUsers);
 
 // Get user by id
-router.get('/:id', userController.getUserById);
+router.get('/:id', AuthMiddleware, isAdmin, userController.getUserById);
 
 module.exports = router;
