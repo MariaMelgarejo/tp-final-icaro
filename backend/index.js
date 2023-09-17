b3c837c2b59a418c16ba3085e304e292846b3622
Daniel Paduani <danielpdn04@gmail.com>
2023-09-17 00:11:33 -0300

    Product: CRUD agregado

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const { notFound, errorHandler } = require('./src/middlewares/errorHandler')
 const usersRouter = require('./src/routes/users');
 const authRouter = require('./src/routes/auth');
 const categoriesRouter = require('./src/routes/categories');
+const productsRouter = require('./src/routes/products');
 
 const db = require('./src/db');
 const sequelize = db.sequelize;
@@ -30,6 +31,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api", authRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/categories", categoriesRouter);
+app.use("/api/products", productsRouter);
 
 // Middlewares
 app.use(notFound);
---
+++ backend/src/controllers/product.js
@@ -0,0 +1,67 @@
+const models = require('../db/models/index');
+const asyncHandler = require('express-async-handler')
+
+// Create product with asyncHandler and valid category
+const createProduct = asyncHandler(async (req, res) => {
+    const category = await models.Category.findByPk(req.body.categoryId)
+    if (!category) throw new Error('La categoría no existe')
+
+    const product = await models.Product.create(req.body)
+    res.status(201).json(
+        {
+            message: 'Product creado',
+            product
+        })
+})
+
+// Get all products
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await models.Product.findAll({ attributes: { exclude: ['updatedAt'] } })
+    res.status(200).json(products)
+})
+
+// Get product by id
+const getProduct = asyncHandler(async (req, res) => {
+    const product = await models.Product.findByPk(req.params?.id, { attributes: { exclude: ['updatedAt'] } })
+    if (!product) throw new Error('El producto no existe')
+    res.status(200).json(product)
+})
+
+// Update product by id
+const updateProduct = asyncHandler(async (req, res) => {
+    const product = await models.Product.findByPk(req.params.id, { attributes: { exclude: ['updatedAt'] } })
+    if (req.body.categoryId) {
+        const category = await models.Category.findByPk(req.body.categoryId)
+        if (!category) throw new Error('La categoría no existe')
+    }
+    if (!product) throw new Error('El producto no existe')
+
+    product.title = req.body.title ?? product.title
+    product.description = req.body.description ?? product.description
+    product.price = req.body.price ?? product.price
+    product.categoryId = req.body.categoryId ?? product.categoryId
+    product.image = req.body.image ?? product.image
+    product.stock = req.body.stock ?? product.stock
+    product.discount = req.body.discount ?? product.discount
+    product.active = req.body.active ?? product.active
+
+    await product.save()
+    res.status(200).json({ message: 'Producto Actualizado!', product })
+})
+
+// Delete product by id
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await models.Product.findByPk(req.params.id)
+    if (!product) throw new Error('El producto no existe')
+
+    await product.destroy()
+    res.status(200).json({ message: 'Producto borrado' })
+})
+
+module.exports = {
+    createProduct,
+    getProducts,
+    getProduct,
+    updateProduct,
+    deleteProduct
+}
