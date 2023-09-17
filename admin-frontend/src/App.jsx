5c227c105a3ba212785f034969dbfd8c857fb1d7
Daniel Paduani <danielpdn04@gmail.com>
2023-09-17 19:38:30 -0300

    Estructura Login, Dashboard y MainLayout

--- admin-frontend/src/App.jsx
+++ admin-frontend/src/App.jsx
@@ -1,7 +1,19 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import MainLayout from "./components/MainLayout";
 
 function App() {
-    return <></>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/admin" element={<MainLayout />}>
+                    <Route index element={<Dashboard />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
